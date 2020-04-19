3b68af9ec7a9eab8d258cac95c7bfdfa55f8054d
harshithva <harshith11032001@gmail.com>
2020-04-19 20:48:22 +0530

    Reviews Controller functions ready

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -32,7 +32,8 @@ class ReviewController extends Controller
      */
     public function create()
     {
-        //
+        $home = Home::first();
+        return view('backend.admin.reviews.create',compact('home'));
     }
 
     /**
@@ -43,7 +44,17 @@ class ReviewController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Review::create(
+            request() -> validate([
+          'title'=> ['required','max:50'],
+          'review'=>['required','max:500'],
+          'client'=>['required','max:50'],
+          'client_info' => ['required','max:50'],
+          'client_img' => ['required','max:255'],
+         ])
+        );
+        Session::flash('message', "Review Created");
+        return redirect()->route('reviews.index');
     }
 
     /**
@@ -65,7 +76,8 @@ class ReviewController extends Controller
      */
     public function edit(Review $review)
     {
-        //
+        $home = Home::first();
+        return view('backend.admin.reviews.edit', compact('home','review'));
     }
 
     /**
@@ -77,7 +89,25 @@ class ReviewController extends Controller
      */
     public function update(Request $request, Review $review)
     {
-        //
+        $this->validate($request,[
+            'title'=> ['required','max:50'],
+          'review'=>['required','max:500'],
+          'client'=>['required','max:50'],
+          'client_info' => ['required','max:50'],
+          'client_img' => ['required','max:255']
+        ]);
+
+        $review->title = $request->title;
+        $review->review = $request->review;
+        $review->client = $request->client;
+        $review->client_info = $request->client_info;
+        $review->client_img = $request->client_img;
+       
+        $review->save();
+        
+        Session::flash('message', "Updated successfully");
+        
+        return redirect()->route('reviews.index');
     }
 
     /**
@@ -88,6 +118,6 @@ class ReviewController extends Controller
      */
     public function destroy(Review $review)
     {
-        //
+        $review->delete();
     }
 }
